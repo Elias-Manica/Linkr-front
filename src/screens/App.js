55e7b3162a6949eabece84eabfb4f613835ee317
Jean Frusca <jeang96@gmail.com>
2022-10-20 15:17:47 -0300

    basic layout implementation of all posts from a user.

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { GlobalStyle } from "../assets/css/GlobalStyle";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
--- src/screens/UserPostsScreen/UserPostsScreen.js
+++ src/screens/UserPostsScreen/UserPostsScreen.js
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import styled from "styled-components";
+import {
+	Container,
+	Header,
+	Logo,
+	SearchBar,
+	UserPostsTitle,
+} from "./styles.js";
+import UserPosts from "../../components/UserPosts.js/UserPosts.js";
 
 export default function UserPostsScreen() {
 	const { id } = useParams();
@@ -33,63 +40,19 @@ export default function UserPostsScreen() {
 					<img src={profileUrl} alt="" />
 					<h1>{username}'s posts</h1>
 				</UserPostsTitle>
+				{userPosts.length === 0
+					? "Efeito de carregamento bonitinho"
+					: userPosts.map((post, index) => {
+							return (
+								<UserPosts
+									post={post}
+									key={index}
+									username={username}
+									profileUrl={profileUrl}
+								/>
+							);
+					  })}
 			</Container>
 		</>
 	);
 }
-
-const UserPostsTitle = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	color: #ffffff;
-	font-size: 43px;
-	line-height: 63px;
-	img {
-		border-radius: 50%;
-		height: 53px;
-		width: 53px;
-		margin: 0px 10px;
-	}
-`;
-
-const Container = styled.div`
-	padding: 0px 10px;
-	background-color: #333333;
-	width: 100%;
-	height: 100vh;
-	margin-top: 72px;
-`;
-const Logo = styled.h1`
-	font-size: 49px;
-	font-weight: 700;
-	line-height: 59.95px;
-	color: #ffffff;
-`;
-
-const SearchBar = styled.div`
-	width: 563px;
-	height: 45px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	border-radius: 8px;
-`;
-
-const Header = styled.div`
-	position: fixed;
-	left: 0;
-	top: 0;
-	padding: 0px 10px;
-	width: 100%;
-	height: 72px;
-	background-color: #151515;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	img {
-		border-radius: 50%;
-		height: 53px;
-		width: 53px;
-	}
-`;
