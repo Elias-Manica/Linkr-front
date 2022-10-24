d595555e23156a75d5f0367bb7994e36d34121f7
elias <eliasmanica@poli.ufrj.br>
2022-10-24 12:27:11 -0300

    all requests to deploy

--- src/services/linkrService.js
+++ src/services/linkrService.js
@@ -4,38 +4,38 @@ const BASE_URL = "http://localhost:4000";
 const DEPLOY_URL = "https://back-projetao-linkr-aefj.herokuapp.com";
 
 function createHeaders(token) {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
-	return config;
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return config;
 }
 
 function postSignUp(body) {
-	const promise = axios.post(`${DEPLOY_URL}/signup`, body);
-	return promise;
+  const promise = axios.post(`${DEPLOY_URL}/signup`, body);
+  return promise;
 }
 
 function getUserPosts(id) {
-	const result = axios.get(`${BASE_URL}/users/${id}`);
-	return result;
+  const result = axios.get(`${DEPLOY_URL}/users/${id}`);
+  return result;
 }
 
 function searchUsers(text) {
-	const promise = axios.post(`${DEPLOY_URL}/users`, { text });
-	return promise;
+  const promise = axios.post(`${DEPLOY_URL}/users`, { text });
+  return promise;
 }
 
 function postLogin(body) {
-	const promise = axios.post(`${DEPLOY_URL}/signin`, body);
-	return promise;
+  const promise = axios.post(`${DEPLOY_URL}/signin`, body);
+  return promise;
 }
 
 function postLogout(token) {
-	const config = createHeaders(token);
-	const promise = axios.delete(`${DEPLOY_URL}/logout`, config);
-	return promise;
+  const config = createHeaders(token);
+  const promise = axios.delete(`${DEPLOY_URL}/logout`, config);
+  return promise;
 }
 
 export { postSignUp, postLogin, postLogout, searchUsers, getUserPosts };
