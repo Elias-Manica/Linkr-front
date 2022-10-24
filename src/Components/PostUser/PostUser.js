6024896229d917c2c368b5c425821bef265e1c1d
Jean Frusca <jeang96@gmail.com>
2022-10-24 16:09:32 -0300

    now a user can like or dislike a post.

--- src/Components/PostUser/PostUser.js
+++ src/Components/PostUser/PostUser.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect, useCallback } from "react";
 
-import { IoMdHeartEmpty } from "react-icons/io";
+import { IoMdHeartEmpty, IoMdHeart } from "react-icons/io";
 import { HiOutlinePencilSquare, HiOutlineTrash } from "react-icons/hi2";
 
-import { deletePost } from "../../services/postService";
+import {
+	deletePost,
+	insertLikePost,
+	removeLikePost,
+} from "../../services/postService";
 
 import {
-  ContainerDelete,
-  ContainerDescription,
-  ContainerEdit,
-  ContainerIconEdit,
-  ContainerImage,
-  ContainerInfosPost,
-  ContainerLink,
-  ContainerNameEdit,
-  ContainerUser,
-  DescriptionPost,
-  NameUser,
-  TextLike,
-  ViewIcon,
-  ViewPost,
+	ContainerDelete,
+	ContainerDescription,
+	ContainerEdit,
+	ContainerIconEdit,
+	ContainerImage,
+	ContainerInfosPost,
+	ContainerLink,
+	ContainerNameEdit,
+	ContainerUser,
+	DescriptionPost,
+	NameUser,
+	TextLike,
+	ViewIcon,
+	ViewPost,
 } from "./styles";
 
 import Microlink from "@microlink/react";
@@ -28,80 +32,91 @@ import ModalDelete from "../ModalDelete/ModalDelete";
 import { ReactTagify } from "react-tagify";
 
 export default function PostUser({ value, getPostsTimeLine, getHashtags }) {
-  const userInfo = JSON.parse(localStorage.getItem("linkr"));
-  const [isOpen, setIsOpen] = useState(false);
-  const [description, setDescription] = useState("");
-  const navigate = useNavigate();
+	const userInfo = JSON.parse(localStorage.getItem("linkr"));
+	const [isOpen, setIsOpen] = useState(false);
+	const [description, setDescription] = useState("");
+	const [liked, setLiked] = useState(false);
+	const navigate = useNavigate();
 
-  function goToUserPage() {
-    navigate(`/users/${value.userid}`);
-  }
+	function goToUserPage() {
+		navigate(`/users/${value.userid}`);
+	}
 
-  function goToHashtagPage(name) {
-    const hashtag = name.substring(1);
+	function goToHashtagPage(name) {
+		const hashtag = name.substring(1);
 
-    navigate(`/hashtag/${hashtag}`);
-  }
+		navigate(`/hashtag/${hashtag}`);
+	}
 
-  const replaceText = useCallback(async () => {
-    const textSepareted = value.text.split(" ");
+	async function likePost() {
+		if (liked) {
+			await removeLikePost(userInfo.token, value.id, userInfo.id);
+			setLiked(false);
+		} else {
+			await insertLikePost(userInfo.token, value.id, userInfo.id);
+			setLiked(true);
+		}
+	}
 
-    if (value.hashtags[0] !== null) {
-      let index = 0;
-      for (let i = 0; i < textSepareted.length; i++) {
-        if (textSepareted[i] === "$#") {
-          textSepareted[i] = `#${value.hashtags[index]}`;
-          index++;
-        }
-      }
-    }
-    setDescription(textSepareted.join(" "));
-  }, [value.hashtags, value.text]);
+	const replaceText = useCallback(async () => {
+		const textSepareted = value.text.split(" ");
 
-  useEffect(() => {
-    replaceText();
-  }, [replaceText]);
+		if (value.hashtags[0] !== null) {
+			let index = 0;
+			for (let i = 0; i < textSepareted.length; i++) {
+				if (textSepareted[i] === "$#") {
+					textSepareted[i] = `#${value.hashtags[index]}`;
+					index++;
+				}
+			}
+		}
+		setDescription(textSepareted.join(" "));
+	}, [value.hashtags, value.text]);
 
-  return (
-    <ViewPost key={value.id}>
-      <ContainerUser>
-        <ContainerImage src={value.pictureurl} onClick={() => goToUserPage()} />
-        <ViewIcon>
-          <IoMdHeartEmpty />
-        </ViewIcon>
-        <TextLike>{value.qtdlikes} likes</TextLike>
-      </ContainerUser>
-      <ContainerInfosPost>
-        <ContainerNameEdit>
-          <NameUser onClick={() => goToUserPage()}>{value.username}</NameUser>
-          <ContainerIconEdit>
-            <ContainerEdit>
-              <HiOutlinePencilSquare />
-            </ContainerEdit>
-            <ContainerDelete onClick={() => setIsOpen(!isOpen)}>
-              <HiOutlineTrash />
-            </ContainerDelete>
-          </ContainerIconEdit>
-        </ContainerNameEdit>
-        <ContainerDescription>
-          <ReactTagify
-            tagClicked={(tag) => goToHashtagPage(tag)}
-            tagStyle={{ color: "white", fontWeight: "bold", cursor: "pointer" }}
-          >
-            <DescriptionPost>{description}</DescriptionPost>
-          </ReactTagify>
-          <ContainerLink>
-            <Microlink url={value.link} />
-          </ContainerLink>
-        </ContainerDescription>
-      </ContainerInfosPost>
-      <ModalDelete
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        postId={value.id}
-        getPostsTimeLine={getPostsTimeLine}
-        getHashtags={getHashtags}
-      />
-    </ViewPost>
-  );
+	useEffect(() => {
+		replaceText();
+	}, [replaceText, value.qtdlikes]);
+
+	return (
+		<ViewPost key={value.id}>
+			<ContainerUser>
+				<ContainerImage src={value.pictureurl} onClick={() => goToUserPage()} />
+				<ViewIcon liked={liked} onClick={() => likePost()}>
+					{liked ? <IoMdHeart /> : <IoMdHeartEmpty />}
+				</ViewIcon>
+				<TextLike>{value.qtdlikes} likes</TextLike>
+			</ContainerUser>
+			<ContainerInfosPost>
+				<ContainerNameEdit>
+					<NameUser onClick={() => goToUserPage()}>{value.username}</NameUser>
+					<ContainerIconEdit>
+						<ContainerEdit>
+							<HiOutlinePencilSquare />
+						</ContainerEdit>
+						<ContainerDelete onClick={() => setIsOpen(!isOpen)}>
+							<HiOutlineTrash />
+						</ContainerDelete>
+					</ContainerIconEdit>
+				</ContainerNameEdit>
+				<ContainerDescription>
+					<ReactTagify
+						tagClicked={(tag) => goToHashtagPage(tag)}
+						tagStyle={{ color: "white", fontWeight: "bold", cursor: "pointer" }}
+					>
+						<DescriptionPost>{description}</DescriptionPost>
+					</ReactTagify>
+					<ContainerLink>
+						<Microlink url={value.link} />
+					</ContainerLink>
+				</ContainerDescription>
+			</ContainerInfosPost>
+			<ModalDelete
+				isOpen={isOpen}
+				setIsOpen={setIsOpen}
+				postId={value.id}
+				getPostsTimeLine={getPostsTimeLine}
+				getHashtags={getHashtags}
+			/>
+		</ViewPost>
+	);
 }
