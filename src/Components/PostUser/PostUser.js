0e678748eb1ad5b84ebdbfe21f325c2464012ed9
Jean Frusca <jeang96@gmail.com>
2022-10-26 02:30:32 -0300

    fixed a bug where the comment was not sent properly. it's now, also, rendering correctly.

--- src/Components/PostUser/PostUser.js
+++ src/Components/PostUser/PostUser.js
@@ -6,32 +6,32 @@ import { TbSend } from "react-icons/tb";
 import { AiOutlineComment } from "react-icons/ai";
 
 import {
-  deletePost,
-  updatePost,
-  insertLikePost,
-  removeLikePost,
-  insertCommentIntoPost,
+	deletePost,
+	updatePost,
+	insertLikePost,
+	removeLikePost,
+	insertCommentIntoPost,
 } from "../../services/postService";
 
 import {
-  ContainerDelete,
-  ContainerDescription,
-  ContainerEdit,
-  ContainerEditInput,
-  ContainerIconEdit,
-  ContainerImage,
-  ContainerInfosPost,
-  ContainerLink,
-  ContainerNameEdit,
-  ContainerUser,
-  DescriptionPost,
-  NameUser,
-  TextLike,
-  ViewIcon,
-  ViewPost,
-  ContainerLOading,
-  CommentsContainer,
-  ViewIconComment,
+	ContainerDelete,
+	ContainerDescription,
+	ContainerEdit,
+	ContainerEditInput,
+	ContainerIconEdit,
+	ContainerImage,
+	ContainerInfosPost,
+	ContainerLink,
+	ContainerNameEdit,
+	ContainerUser,
+	DescriptionPost,
+	NameUser,
+	TextLike,
+	ViewIcon,
+	ViewPost,
+	ContainerLOading,
+	CommentsContainer,
+	ViewIconComment,
 } from "./styles";
 
 import Microlink from "@microlink/react";
@@ -43,183 +43,172 @@ import { Oval } from "react-loader-spinner";
 import Comments from "../Comments/Comments";
 
 export default function PostUser({ value, getPostsTimeLine, getHashtags }) {
-  const userInfo = JSON.parse(localStorage.getItem("linkr"));
-  const [isOpen, setIsOpen] = useState(false);
-  const [description, setDescription] = useState("");
-  const [edit, setEdit] = useState(false);
-  const [putDescription, setPutDescription] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [liked, setLiked] = useState(false);
-  const [commentText, setCommentText] = useState("");
-  const [seeComment, setSeeComment] = useState(false);
-  const navigate = useNavigate();
+	const userInfo = JSON.parse(localStorage.getItem("linkr"));
+	const [isOpen, setIsOpen] = useState(false);
+	const [description, setDescription] = useState("");
+	const [edit, setEdit] = useState(false);
+	const [putDescription, setPutDescription] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [liked, setLiked] = useState(false);
+	const [commentText, setCommentText] = useState("");
+	const [seeComment, setSeeComment] = useState(false);
+	const navigate = useNavigate();
 
-  const inputRef = useRef(null);
+	const inputRef = useRef(null);
 
-  console.log(value, " value");
+	async function editPost(token, id) {
+		setLoading(true);
+		const body = {
+			link: value.link,
+			text: putDescription,
+		};
+		try {
+			const response = await updatePost(token, body, id);
+			getPostsTimeLine();
+			setLoading(false);
+		} catch (error) {
+			alert("An error occured during edit post");
+			setLoading(false);
+		}
+	}
 
-  async function editPost(token, id) {
-    setLoading(true);
-    const body = {
-      link: value.link,
-      text: putDescription,
-    };
-    try {
-      const response = await updatePost(token, body, id);
-      getPostsTimeLine();
-      setLoading(false);
-    } catch (error) {
-      alert("An error occured during edit post");
-      setLoading(false);
-    }
-  }
+	async function openTextEdit(e) {
+		e.preventDefault();
+		if (edit === true && putDescription !== description) {
+			editPost(userInfo.token, value.id);
+			return;
+		}
+		setEdit(!edit);
+	}
 
-  async function openTextEdit(e) {
-    e.preventDefault();
-    if (edit === true && putDescription !== description) {
-      editPost(userInfo.token, value.id);
-      return;
-    }
-    setEdit(!edit);
-  }
+	function goToUserPage() {
+		navigate(`/users/${value.userid}`);
+	}
 
-  function goToUserPage() {
-    navigate(`/users/${value.userid}`);
-  }
+	function goToHashtagPage(name) {
+		const hashtag = name.substring(1);
 
-  function goToHashtagPage(name) {
-    const hashtag = name.substring(1);
+		navigate(`/hashtag/${hashtag}`);
+	}
 
-    navigate(`/hashtag/${hashtag}`);
-  }
+	async function likePost() {
+		if (liked) {
+			await removeLikePost(userInfo.token, value.id, userInfo.id);
+			setLiked(false);
+		} else {
+			await insertLikePost(userInfo.token, value.id, userInfo.id);
+			setLiked(true);
+		}
+	}
 
-  async function commentPost(e) {
-    e.preventDefault();
-    await insertCommentIntoPost(value.id, userInfo.token, commentText);
-  }
+	const replaceText = useCallback(async () => {
+		const textSepareted = value.text.split(" ");
 
-  function handleInput(e) {
-    setCommentText(e.target.value);
-  }
+		if (value.hashtags[0] !== null) {
+			let index = 0;
+			for (let i = 0; i < textSepareted.length; i++) {
+				if (textSepareted[i] === "$#") {
+					textSepareted[i] = `#${value.hashtags[index]}`;
+					index++;
+				}
+			}
+		}
+		setDescription(textSepareted.join(" "));
+		setPutDescription(textSepareted.join(" "));
+	}, [value.hashtags, value.text]);
 
-  async function likePost() {
-    if (liked) {
-      await removeLikePost(userInfo.token, value.id, userInfo.id);
-      setLiked(false);
-    } else {
-      await insertLikePost(userInfo.token, value.id, userInfo.id);
-      setLiked(true);
-    }
-  }
+	useEffect(() => {
+		replaceText();
 
-  const replaceText = useCallback(async () => {
-    const textSepareted = value.text.split(" ");
+		if (edit) {
+			inputRef.current.focus();
+		}
+	}, [replaceText, edit]);
 
-    if (value.hashtags[0] !== null) {
-      let index = 0;
-      for (let i = 0; i < textSepareted.length; i++) {
-        if (textSepareted[i] === "$#") {
-          textSepareted[i] = `#${value.hashtags[index]}`;
-          index++;
-        }
-      }
-    }
-    setDescription(textSepareted.join(" "));
-    setPutDescription(textSepareted.join(" "));
-  }, [value.hashtags, value.text]);
+	return (
+		<>
+			<ViewPost key={value.id}>
+				<ContainerUser>
+					<ContainerImage
+						src={value.pictureurl}
+						onClick={() => goToUserPage()}
+					/>
+					<ViewIcon liked={liked} onClick={() => likePost()}>
+						{liked ? <IoMdHeart /> : <IoMdHeartEmpty />}
+					</ViewIcon>
+					<TextLike>{value.qtdlikes} likes</TextLike>
+					<ViewIconComment onClick={() => setSeeComment(!seeComment)}>
+						<AiOutlineComment />
+					</ViewIconComment>
+					<TextLike>{value.qtdcomments} comments</TextLike>
+				</ContainerUser>
+				<ContainerInfosPost>
+					<form>
+						<ContainerNameEdit>
+							<NameUser onClick={() => goToUserPage()}>
+								{value.username}
+							</NameUser>
+							{userInfo.userid === value.userid ? (
+								<ContainerIconEdit>
+									<ContainerEdit onClick={(e) => openTextEdit(e)}>
+										<HiOutlinePencilSquare />
+									</ContainerEdit>
+									<ContainerDelete onClick={() => setIsOpen(!isOpen)}>
+										<HiOutlineTrash />
+									</ContainerDelete>
+								</ContainerIconEdit>
+							) : null}
+						</ContainerNameEdit>
+						<ContainerDescription>
+							{loading ? (
+								<ContainerLOading>
+									<Oval
+										height={30}
+										width={30}
+										color="#171717"
+										wrapperStyle={{}}
+										wrapperClass=""
+										visible={true}
+										ariaLabel="oval-loading"
+										secondaryColor="#FFFFFF"
+										strokeWidth={2}
+										strokeWidthSecondary={2}
+									/>
+								</ContainerLOading>
+							) : edit ? (
+								<ContainerEditInput
+									value={putDescription}
+									onChange={(e) => setPutDescription(e.target.value)}
+									ref={inputRef}
+								/>
+							) : (
+								<ReactTagify
+									tagClicked={(tag) => goToHashtagPage(tag)}
+									tagStyle={{
+										color: "white",
+										fontWeight: "bold",
+										cursor: "pointer",
+									}}
+								>
+									<DescriptionPost>{description}</DescriptionPost>
+								</ReactTagify>
+							)}
 
-  useEffect(() => {
-    replaceText();
-
-    if (edit) {
-      inputRef.current.focus();
-    }
-  }, [replaceText, edit]);
-
-  return (
-    <>
-      <ViewPost key={value.id}>
-        <ContainerUser>
-          <ContainerImage
-            src={value.pictureurl}
-            onClick={() => goToUserPage()}
-          />
-          <ViewIcon liked={liked} onClick={() => likePost()}>
-            {liked ? <IoMdHeart /> : <IoMdHeartEmpty />}
-          </ViewIcon>
-          <TextLike>{value.qtdlikes} likes</TextLike>
-          <ViewIconComment onClick={() => setSeeComment(!seeComment)}>
-            <AiOutlineComment />
-          </ViewIconComment>
-          <TextLike>{value.qtdcomments} comments</TextLike>
-        </ContainerUser>
-        <ContainerInfosPost>
-          <form>
-            <ContainerNameEdit>
-              <NameUser onClick={() => goToUserPage()}>
-                {value.username}
-              </NameUser>
-              {userInfo.userid === value.userid ? (
-                <ContainerIconEdit>
-                  <ContainerEdit onClick={(e) => openTextEdit(e)}>
-                    <HiOutlinePencilSquare />
-                  </ContainerEdit>
-                  <ContainerDelete onClick={() => setIsOpen(!isOpen)}>
-                    <HiOutlineTrash />
-                  </ContainerDelete>
-                </ContainerIconEdit>
-              ) : null}
-            </ContainerNameEdit>
-            <ContainerDescription>
-              {loading ? (
-                <ContainerLOading>
-                  <Oval
-                    height={30}
-                    width={30}
-                    color="#171717"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                    visible={true}
-                    ariaLabel="oval-loading"
-                    secondaryColor="#FFFFFF"
-                    strokeWidth={2}
-                    strokeWidthSecondary={2}
-                  />
-                </ContainerLOading>
-              ) : edit ? (
-                <ContainerEditInput
-                  value={putDescription}
-                  onChange={(e) => setPutDescription(e.target.value)}
-                  ref={inputRef}
-                />
-              ) : (
-                <ReactTagify
-                  tagClicked={(tag) => goToHashtagPage(tag)}
-                  tagStyle={{
-                    color: "white",
-                    fontWeight: "bold",
-                    cursor: "pointer",
-                  }}
-                >
-                  <DescriptionPost>{description}</DescriptionPost>
-                </ReactTagify>
-              )}
-
-              <ContainerLink>
-                <Microlink url={value.link} />
-              </ContainerLink>
-            </ContainerDescription>
-          </form>
-        </ContainerInfosPost>
-        <ModalDelete
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-          postId={value.id}
-          getPostsTimeLine={getPostsTimeLine}
-          getHashtags={getHashtags}
-        />
-      </ViewPost>
-      {seeComment ? <Comments value={value} /> : null}
-    </>
-  );
+							<ContainerLink>
+								<Microlink url={value.link} />
+							</ContainerLink>
+						</ContainerDescription>
+					</form>
+				</ContainerInfosPost>
+				<ModalDelete
+					isOpen={isOpen}
+					setIsOpen={setIsOpen}
+					postId={value.id}
+					getPostsTimeLine={getPostsTimeLine}
+					getHashtags={getHashtags}
+				/>
+			</ViewPost>
+			{seeComment ? <Comments value={value} /> : null}
+		</>
+	);
 }
