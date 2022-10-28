f54c47a3acff37cc1f347fc97e974ee41f7f82af
Jean Frusca <jeang96@gmail.com>
2022-10-28 02:54:18 -0300

    fix: changed to DEPLOY URL on the function to publish a post.

--- src/screens/NewPost/NewPost.js
+++ src/screens/NewPost/NewPost.js
@@ -3,98 +3,98 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const NewPost = ({ getPostsTimeLine, getHashtags }) => {
-  const [publishing, setPublishing] = useState(true);
-  const [textInput, setTextInput] = useState("");
-  const [urlInput, setUrlInput] = useState("");
-  const userInfo = JSON.parse(localStorage.getItem("linkr"));
+	const [publishing, setPublishing] = useState(true);
+	const [textInput, setTextInput] = useState("");
+	const [urlInput, setUrlInput] = useState("");
+	const userInfo = JSON.parse(localStorage.getItem("linkr"));
 
-  function click() {
-    if (textInput.length === 0) return;
-    if (urlInput.length === 0) return;
-    if (publishing === false) return;
-    setPublishing(false);
+	function click() {
+		if (textInput.length === 0) return;
+		if (urlInput.length === 0) return;
+		if (publishing === false) return;
+		setPublishing(false);
 
-    const headers = {
-      authorization: JSON.parse(localStorage.getItem("linkr")).token,
-    };
-    axios
-      .post(
-        "http://localhost:4000/publish",
-        { text: textInput, link: urlInput },
-        { headers }
-      )
-      .then((res) => {
-        setPublishing(true);
-        setTextInput("");
-        setUrlInput("");
-        getPostsTimeLine(0);
-        getHashtags();
-      })
-      .catch((res) => {
-        console.log(res);
-        setPublishing(true);
-        setTextInput("");
-        setUrlInput("");
-        alert(res);
-      });
-  }
+		const headers = {
+			authorization: JSON.parse(localStorage.getItem("linkr")).token,
+		};
+		axios
+			.post(
+				"https://back-projetao-linkr-aefj.herokuapp.com/publish",
+				{ text: textInput, link: urlInput },
+				{ headers }
+			)
+			.then((res) => {
+				setPublishing(true);
+				setTextInput("");
+				setUrlInput("");
+				getPostsTimeLine(0);
+				getHashtags();
+			})
+			.catch((res) => {
+				console.log(res);
+				setPublishing(true);
+				setTextInput("");
+				setUrlInput("");
+				alert(res);
+			});
+	}
 
-  function input(e, type) {
-    if (type === "url") setUrlInput(e.target.value);
-    if (type === "text") setTextInput(e.target.value);
-  }
+	function input(e, type) {
+		if (type === "url") setUrlInput(e.target.value);
+		if (type === "text") setTextInput(e.target.value);
+	}
 
-  return (
-    <Css.NewPost>
-      <Css.ProfileLabel>
-        <Css.ProfilePost src={userInfo.pictureurl} alt="user-picture" />
-      </Css.ProfileLabel>
-      <Css.PostLabel>
-        <h1>What are you going to share today?</h1>
-        {publishing ? (
-          <>
-            <Css.InputUrl
-              disabled={false}
-              value={urlInput}
-              type="url"
-              placeholder="http://"
-              onChange={(e) => input(e, "url")}
-            />
-            <Css.InputText
-              disabled={false}
-              value={textInput}
-              type="text"
-              placeholder="Awesome article about #javascript"
-              onChange={(e) => input(e, "text")}
-            />
-          </>
-        ) : (
-          <>
-            <Css.InputUrl
-              disabled={true}
-              value={urlInput}
-              type="url"
-              placeholder="http://"
-              onChange={(e) => input(e, "url")}
-            />
-            <Css.InputText
-              disabled={true}
-              value={textInput}
-              type="text"
-              placeholder="Awesome article about #javascript"
-              onChange={(e) => input(e, "text")}
-            />
-          </>
-        )}
+	return (
+		<Css.NewPost>
+			<Css.ProfileLabel>
+				<Css.ProfilePost src={userInfo.pictureurl} alt="user-picture" />
+			</Css.ProfileLabel>
+			<Css.PostLabel>
+				<h1>What are you going to share today?</h1>
+				{publishing ? (
+					<>
+						<Css.InputUrl
+							disabled={false}
+							value={urlInput}
+							type="url"
+							placeholder="http://"
+							onChange={(e) => input(e, "url")}
+						/>
+						<Css.InputText
+							disabled={false}
+							value={textInput}
+							type="text"
+							placeholder="Awesome article about #javascript"
+							onChange={(e) => input(e, "text")}
+						/>
+					</>
+				) : (
+					<>
+						<Css.InputUrl
+							disabled={true}
+							value={urlInput}
+							type="url"
+							placeholder="http://"
+							onChange={(e) => input(e, "url")}
+						/>
+						<Css.InputText
+							disabled={true}
+							value={textInput}
+							type="text"
+							placeholder="Awesome article about #javascript"
+							onChange={(e) => input(e, "text")}
+						/>
+					</>
+				)}
 
-        <Css.BoxButton>
-          <Css.ButtonPublish onClick={click}>
-            {publishing ? "Publish" : "Publishing..."}
-          </Css.ButtonPublish>
-        </Css.BoxButton>
-      </Css.PostLabel>
-    </Css.NewPost>
-  );
+				<Css.BoxButton>
+					<Css.ButtonPublish onClick={click}>
+						{publishing ? "Publish" : "Publishing..."}
+					</Css.ButtonPublish>
+				</Css.BoxButton>
+			</Css.PostLabel>
+		</Css.NewPost>
+	);
 };
 
 export default NewPost;
